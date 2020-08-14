c4239b6b9afe9b552d078f2ac0c08be338d7cb4b
Puja <jain.puja94@gmail.com>
2020-08-14 12:23:31 +0530

    Checking in all folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { HomePageComponent } from "./components/home-page/home-page.component";
+import { FirstPageComponent } from "./components/first-page/first-page.component";
+import { LeftPanelComponent } from "./components/left-panel/left-panel.component";
+import { RecordComponent } from './components/home-page/record/record.component';
+
+const appRoutes: Routes = [
+  { path: "", component: FirstPageComponent },
+  { path: "firstPage", component: FirstPageComponent },
+  { path: "homePage", component: HomePageComponent, children: [
+    {path: '', component: RecordComponent}
+  ] },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomePageComponent,
+    FirstPageComponent,
+    LeftPanelComponent,
+    RecordComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    BrowserModule, 
+    AppRoutingModule, 
+    RouterModule.forRoot(appRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
