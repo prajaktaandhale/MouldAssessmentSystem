aaa97351de7e2fed27dace3639f7470887471454
prajakta andhale <Prajakta.Andhale@sprint.com>
2020-08-19 15:33:09 +0530

    login Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-
+import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
@@ -9,6 +10,17 @@ import { HomePageComponent } from "./components/home-page/home-page.component";
 import { FirstPageComponent } from "./components/first-page/first-page.component";
 import { LeftPanelComponent } from "./components/left-panel/left-panel.component";
 import { RecordComponent } from './components/home-page/record/record.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  MatCardModule,
+  MatTabsModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatIconModule
+} from '@angular/material';
 
 const appRoutes: Routes = [
   { path: "", component: FirstPageComponent },
@@ -29,8 +41,19 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule, 
-    AppRoutingModule, 
-    RouterModule.forRoot(appRoutes)],
+    AppRoutingModule,
+    MatToolbarModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    RouterModule.forRoot(appRoutes), BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
